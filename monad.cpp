2f3575eaadd2641ac9e824cb581476d63fac76fc
Zach Laine <whatwasthataddress@gmail.com>
2014-05-08 17:14:06 -0500

    Move maybe to its own header.

--- monad.cpp
+++ monad.cpp
@@ -1,76 +1,7 @@
-#include "monad.hpp"
+#include "maybe.hpp"
 
 #include <iostream>
 
-
-struct nothing_t {};
-const nothing_t nothing = {};
-
-namespace monad {
-
-    template <typename T>
-    struct monad<T, bool>
-    {
-        using this_type = monad<T, bool>;
-        using value_type = T;
-        using state_type = bool;
-
-        monad () :
-            value_ {},
-            state_ {}
-        {}
-
-        monad (value_type value, state_type state) :
-            value_ {value},
-            state_ {state}
-        {}
-
-        monad (T t) :
-            value_ {t},
-            state_ {true}
-        {}
-
-        monad (nothing_t) :
-            value_ {},
-            state_ {false}
-        {}
-
-        monad (const monad& rhs) = default;
-        monad& operator= (const monad& rhs) = default;
-
-        value_type value () const
-        { return value_; }
-
-        state_type state () const
-        { return state_; }
-
-        template <typename Fn>
-        this_type bind (Fn f) const
-        {
-            if (!state_)
-                return *this;
-            else
-                return f(value_);
-        }
-
-        template <typename State_>
-        join_result_t<this_type, State_> join() const
-        { return !state_ ? value_type{nothing} : value_; }
-
-        value_type & mutable_value ()
-        { return value_; }
-
-        state_type & mutable_state ()
-        { return state_; }
-
-        value_type value_;
-        state_type state_;
-    };
-
-}
-
-template <typename T>
-using maybe = monad::monad<T, bool>;
 
 MONAD_TEMPLATE_BINARY_OP(+, maybe, 1);
 
@@ -81,16 +12,6 @@ T add3 (T l, T m, T r)
 template <typename T>
 T add_2 (T t)
 { return t + 2; }
-
-template <typename T>
-std::ostream& operator<< (std::ostream& os, maybe<T> m)
-{
-    if (!m.state_)
-        os << "Nothing";
-    else
-        os << "Just " << m.value_;
-    return os;
-}
 
 template <typename T>
 std::ostream& operator<< (std::ostream& os, maybe<std::vector<T>> m)
